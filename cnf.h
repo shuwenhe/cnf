66d5d12163ac57c223c28a12cfbce9a35e09112c
ShuwenHe <1201220707@pku.edu.cn>
2025-01-22 11:34:02 +0800

    feat:cnf

---
+++ cnf.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* host = "139.10.159.40";
+const char* user = "shuwen";
+const char* pwd = "123@_00..";
+const char* db = "shuwen";
+const int port = 3306;
+
+#endif
