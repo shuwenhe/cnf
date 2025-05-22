cc86afc725c8d5baeaf4fad8295810fd9aecbbc2
Shuwen He <1201220707@pku.edu.cn>
2025-05-22 11:41:57 +0800

    fix:host

--- cnf.h
+++ cnf.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* host = "139.10.159.40";
+const char* host = "127.0.0.1";
 const char* user = "shuwen";
 const char* pwd = "123@_00..";
 const char* db = "shuwen";
